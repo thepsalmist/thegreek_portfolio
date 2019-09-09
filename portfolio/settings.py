68dacc989adac31fe3ffdc51e091a02bd2d0899c
thepsalmist <xf.xavierfrank@gmail.com>
2019-09-09 10:56:43 +0300

    Updated settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "nqpam(uy7_o4m#1nv17h7qtou*)n&-3s@+ee*_1#vbv(d4(f2-"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thegreek.herokuapp.com']
 
 
 # Application definition
