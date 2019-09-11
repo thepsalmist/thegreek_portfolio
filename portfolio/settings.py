c09218291561286a5e4a6e5e2088389995bcc6ce
thepsalmist <xf.xavierfrank@gmail.com>
2019-09-11 12:38:14 +0300

    modify settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,6 +126,8 @@ AWS_S3_REGION_NAME = "us-east-2"
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
